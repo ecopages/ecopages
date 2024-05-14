7118fd3ed137e4a5c15106e340a37e16183ff826
Andrea Zanenghi <andeeplus@gmail.com>
2024-05-15 00:07:39 +0200

    feat(#21): fixed issue with absolute imports instead of workspace imports

--- packages/core/fixtures/app/eco.config.ts
+++ packages/core/fixtures/app/eco.config.ts
@@ -1,7 +1,7 @@
 import type { EcoPagesConfigInput } from '@eco-pages/core';
-import { kitajsPlugin } from '../../../integrations/kitajs/src';
-import { litPlugin } from '../../../integrations/lit/src';
-import { mdxPlugin } from '../../../integrations/mdx/src';
+import { kitajsPlugin } from '@eco-pages/kitajs';
+import { litPlugin } from '@eco-pages/lit/src';
+import { mdxPlugin } from '@eco-pages/mdx/src';
 
 const config: EcoPagesConfigInput = {
   rootDir: import.meta.dir,

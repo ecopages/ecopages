ac4d8d63cea36503f01cdc70968fa1dce5bb3e19
Andrea Zanenghi <andeeplus@gmail.com>
2024-05-25 17:04:03 +0200

    feat(#33): removed spec from dist in lite-elements

--- packages/lite-elements/happydom.ts
+++ packages/lite-elements/happydom.ts
@@ -1,3 +1,8 @@
+import { afterAll } from 'bun:test';
 import { GlobalRegistrator } from '@happy-dom/global-registrator';
 
 GlobalRegistrator.register();
+
+afterAll(() => {
+  GlobalRegistrator.unregister();
+});

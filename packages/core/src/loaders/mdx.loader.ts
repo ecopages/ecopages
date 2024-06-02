af679475d27eae486ad946f0f09fb1c70b690547
Andrea Zanenghi <andeeplus@gmail.com>
2024-06-02 22:23:04 +0200

    feat(#44): updated packages.json references

--- packages/core/src/loaders/mdx.loader.ts
+++ packages/core/src/loaders/mdx.loader.ts
@@ -1,8 +1,6 @@
-import { appLogger } from '@/utils/app-logger';
 import mdx from '@mdx-js/esbuild';
 import { plugin } from 'bun';
 
-appLogger.debug('Setting up mdx-loader');
 plugin(
   // @ts-expect-error: esbuild plugin vs bun plugin
   mdx({

923d79cf8a69fab3e58fe859f5cb268452b31b52
Andrea Zanenghi <andeeplus@gmail.com>
2024-07-01 23:14:37 +0200

    Removed changeset

--- scripts/jsr-sync-version.ts
+++ scripts/jsr-sync-version.ts
@@ -1,12 +1,16 @@
+import rootPackage from '../package.json';
+
+if (!rootPackage.version) {
+  throw new Error('Root package.json does not have a version');
+}
 const glob = new Bun.Glob('packages/**/*/jsr.json');
 
 for await (const jsrJson of glob.scan()) {
   const packageJson = jsrJson.replace('jsr.json', 'package.json');
-  const packageConfig = await Bun.file(packageJson).json();
-  const packageVersion = packageConfig.version;
+  const modifiedPackageJsonConfig = await Bun.file(packageJson).json();
+  modifiedPackageJsonConfig.version = rootPackage.version;
   const jsrConfig = await Bun.file(jsrJson).text();
-  const modifiedConfig = jsrConfig.replaceAll('${version}', packageVersion);
-  await Bun.write(jsrJson, modifiedConfig);
+  const modifiedJsrConfig = jsrConfig.replaceAll('${version}', modifiedPackageJsonConfig.version);
+  await Bun.write(packageJson, modifiedPackageJsonConfig);
+  await Bun.write(jsrJson, modifiedJsrConfig);
 }
-
-export type {};

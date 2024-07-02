22533ac3a0f4bed14ea6bab6cf399d7683e0e503
Andrea Zanenghi <andeeplus@gmail.com>
2024-07-02 23:53:12 +0200

    Updated jsr sync version

--- scripts/jsr-sync-version.ts
+++ scripts/jsr-sync-version.ts
@@ -7,10 +7,15 @@ const glob = new Bun.Glob('packages/**/*/jsr.json');
 
 for await (const jsrJson of glob.scan()) {
   const packageJson = jsrJson.replace('jsr.json', 'package.json');
+
   const modifiedPackageJsonConfig = await Bun.file(packageJson).json();
   modifiedPackageJsonConfig.version = rootPackage.version;
+
   const modifiedJsrConfig = await Bun.file(jsrJson).json();
   modifiedJsrConfig.version = rootPackage.version;
-  await Bun.write(packageJson, JSON.stringify(modifiedPackageJsonConfig, null, 2));
-  await Bun.write(jsrJson, JSON.stringify(modifiedJsrConfig, null, 2));
+
+  await Promise.all([
+    Bun.write(packageJson, JSON.stringify(modifiedPackageJsonConfig, null, 2)),
+    Bun.write(jsrJson, JSON.stringify(modifiedJsrConfig, null, 2)),
+  ]);
 }

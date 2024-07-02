8ebabd0e2e92ec540aaa80b6ca48466235d1b71f
Andrea Zanenghi <andeeplus@gmail.com>
2024-07-02 22:16:50 +0200

    bumped jsr version to 0.1.1, update sync script

--- scripts/jsr-sync-version.ts
+++ scripts/jsr-sync-version.ts
@@ -9,8 +9,8 @@ for await (const jsrJson of glob.scan()) {
   const packageJson = jsrJson.replace('jsr.json', 'package.json');
   const modifiedPackageJsonConfig = await Bun.file(packageJson).json();
   modifiedPackageJsonConfig.version = rootPackage.version;
-  const jsrConfig = await Bun.file(jsrJson).text();
-  const modifiedJsrConfig = jsrConfig.replaceAll('${version}', modifiedPackageJsonConfig.version);
+  const modifiedJsrConfig = await Bun.file(jsrJson).json();
+  modifiedJsrConfig.version = rootPackage.version;
   await Bun.write(packageJson, JSON.stringify(modifiedPackageJsonConfig, null, 2));
-  await Bun.write(jsrJson, modifiedJsrConfig);
+  await Bun.write(jsrJson, JSON.stringify(modifiedJsrConfig, null, 2));
 }

b6ee2402ea946a257fb005ee6ad67a734283f3b5
Andrea Zanenghi <andeeplus@gmail.com>
2024-09-10 01:46:04 +0200

    fixed type return missing for jsr correct publish

--- packages/core/src/utils/css.ts
+++ packages/core/src/utils/css.ts
@@ -1,6 +1,6 @@
 import { PostCssProcessor } from '@ecopages/postcss-processor';
 
-export const css = async (strings: TemplateStringsArray, ...values: any[]) => {
+export const css = async (strings: TemplateStringsArray, ...values: any[]): Promise<string> => {
   const css = strings.reduce((acc, str, i) => acc + str + (values[i] || ''), '');
   return await PostCssProcessor.processStringOrBuffer(css);
 };

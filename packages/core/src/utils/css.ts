08b552f4370ee9d71476d51065f9941739b16c80
Andrea Zanenghi <andeeplus@gmail.com>
2025-04-11 00:45:05 +0200

    chore: temp fix issue with postcss circular ref

--- packages/core/src/utils/css.ts
+++ packages/core/src/utils/css.ts
@@ -1,6 +1,5 @@
-import { PostCssProcessor } from '@ecopages/postcss-processor';
-
 export const css = async (strings: TemplateStringsArray, ...values: any[]): Promise<string> => {
   const css = strings.reduce((acc, str, i) => acc + str + (values[i] || ''), '');
-  return await PostCssProcessor.processStringOrBuffer(css);
+  // return await PostCssProcessor.processStringOrBuffer(css);
+  return css;
 };

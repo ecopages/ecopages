b623ca88b5543af1c7941fabb05d54decc5d0864
Andrea Zanenghi <andeeplus@gmail.com>
2025-04-11 21:37:15 +0200

    feature(81) server config + routes

---
+++ playground/manual/eco.config.ts
@@ -0,0 +1,35 @@
+// import '@ecopages/bun-postcss-loader';
+import '@ecopages/bun-mdx-kitajs-loader';
+import path from 'node:path';
+import { ConfigBuilder } from '@ecopages/core';
+import { imageProcessorPlugin } from '@ecopages/image-processor';
+import { kitajsPlugin } from '@ecopages/kitajs';
+import { litPlugin } from '@ecopages/lit';
+import { mdxPlugin } from '@ecopages/mdx';
+// import { postcssProcessorPlugin } from '@ecopages/postcss-processor';
+
+export default await new ConfigBuilder()
+  .setRootDir(import.meta.dir)
+  .setBaseUrl(import.meta.env.ECOPAGES_BASE_URL)
+  .setIntegrations([kitajsPlugin(), litPlugin(), mdxPlugin()])
+  .setError404Template('404.kita.tsx')
+  .setProcessors([
+    imageProcessorPlugin({
+      options: {
+        sourceDir: path.resolve(import.meta.dir, 'src/images'),
+        outputDir: path.resolve(import.meta.dir, '.eco/public/images'),
+        publicPath: '/public/images',
+        acceptedFormats: ['jpg', 'jpeg', 'png', 'webp'],
+        quality: 80,
+        format: 'webp',
+        sizes: [
+          { width: 320, label: 'sm' },
+          { width: 768, label: 'md' },
+          { width: 1024, label: 'lg' },
+          { width: 1920, label: 'xl' },
+        ],
+      },
+    }),
+    // postcssProcessorPlugin(),
+  ])
+  .build();
---
+++ playground/manual/src/pages/labs/client-images.script.ts
@@ -0,0 +1,84 @@
+import {
+  ezi76Gu53NklsuUnsplashJpg,
+  smallJpg,
+  theodorePoncetQzephogqd7WUnsplashJpg,
+  urbanVintage78A265Wpio4UnsplashJpg,
+} from 'ecopages:images';
+import { html } from '@ecopages/core/html';
+import { EcoImage } from '@ecopages/image-processor/component/html';
+import { RadiantElement } from '@ecopages/radiant/core';
+import { customElement, onEvent, query } from '@ecopages/radiant/decorators';
+
+@customElement('eco-images')
+export class EcoImages extends RadiantElement {
+  @query({ ref: 'container' }) container!: HTMLElement;
+
+  override connectedCallback(): void {
+    super.connectedCallback();
+    this.createRandomImage();
+  }
+
+  @onEvent({ ref: 'create-img', type: 'click' })
+  createRandomImage() {
+    const availableImages = [
+      urbanVintage78A265Wpio4UnsplashJpg,
+      theodorePoncetQzephogqd7WUnsplashJpg,
+      ezi76Gu53NklsuUnsplashJpg,
+      smallJpg,
+    ];
+
+    const randomImage = availableImages[Math.floor(Math.random() * availableImages.length)];
+
+    this.renderTemplate({
+      target: this.container,
+      template: html`
+				!${EcoImage({
+          ...randomImage,
+          alt: 'Random image',
+          layout: 'full-width',
+          height: 200,
+          priority: true,
+        })}
+				!${EcoImage({
+          ...randomImage,
+          alt: 'Random image',
+          width: 600,
+          height: 200,
+          layout: 'constrained',
+          priority: false,
+        })}
+				!${EcoImage({
+          ...randomImage,
+          alt: 'Random image',
+          layout: 'fixed',
+          width: 200,
+          height: 200,
+          priority: false,
+        })}
+				!${EcoImage({
+          ...randomImage,
+          width: 400,
+          alt: 'Random image',
+          priority: false,
+          unstyled: true,
+          'data-test': 'attribute',
+        })}
+				!${EcoImage({
+          ...randomImage,
+          alt: 'Random image',
+          priority: false,
+          width: 300,
+          aspectRatio: '1/2',
+        })}
+			`,
+    });
+  }
+}
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'eco-images': HtmlTag;
+    }
+  }
+}

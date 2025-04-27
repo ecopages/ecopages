86dfc8de962fd655ad7e274e11818e8889bc26d3
Andrea Zanenghi <andeeplus@gmail.com>
2025-04-27 17:57:40 +0200

    chore: fixed slow types

--- packages/core/src/constants.ts
+++ packages/core/src/constants.ts
@@ -21,13 +21,13 @@ export const IS_BUN = typeof Bun !== 'undefined';
 /**
  * Directory used for storing assets.
  */
-export const RESOLVED_ASSETS_DIR = 'assets';
+export const RESOLVED_ASSETS_DIR: string = 'assets';
 
 /**
  * Directory used for storing vendor assets.
  * This is a subdirectory of the assets directory.
  */
-export const RESOLVED_ASSETS_VENDORS_DIR = `${RESOLVED_ASSETS_DIR}/vendors`;
+export const RESOLVED_ASSETS_VENDORS_DIR: string = `${RESOLVED_ASSETS_DIR}/vendors`;
 
 interface GeneratedPathOptions {
   root: string;

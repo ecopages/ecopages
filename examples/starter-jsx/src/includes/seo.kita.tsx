997c70a2a0fcaefd128c06914b3296c9e1b5f4bb
azanenghi <andrea.zanenghi@gmail.com>
2026-02-01 00:10:11 +0100

    feat(starter-jsx): cleanup starter jsx

--- examples/starter-jsx/src/includes/seo.kita.tsx
+++ examples/starter-jsx/src/includes/seo.kita.tsx
@@ -6,10 +6,10 @@ export function Seo({ title, description, url, keywords }: PageMetadataProps) {
 	return (
 		<>
 			<title safe>{title}</title>
-			<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-			<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-			<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-			<link rel="manifest" href="/favicon/site.webmanifest" />
+			<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+			<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+			<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+			<link rel="manifest" href="/site.webmanifest" />
 			<link rel="robots" href="/robots.txt" />
 			<meta name="description" content={description} />
 			{keywords?.length ? ((<meta name="keywords" content={keywords.join(',')} />) as 'safe') : null}

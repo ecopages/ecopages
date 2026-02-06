4477946b45cb33084da2d95a65ae8c6ab098d652
azanenghi <andrea.zanenghi@gmail.com>
2026-02-06 19:31:10 +0100

    feat: add styles.css export to browser-router and react-router

--- packages/react-router/src/props-script.tsx
+++ packages/react-router/src/props-script.tsx
@@ -1,4 +1,4 @@
-import { type FC } from 'react';
+import { createElement, type FC } from 'react';
 
 export interface EcoPropsScriptProps {
 	/** The page props to serialize for client-side hydration */
@@ -11,11 +11,9 @@ export interface EcoPropsScriptProps {
  * Using application/json allows direct parsing without regex.
  */
 export const EcoPropsScript: FC<EcoPropsScriptProps> = ({ data }) => {
-	return (
-		<script
-			id="__ECO_PAGE_DATA__"
-			type="application/json"
-			dangerouslySetInnerHTML={{ __html: JSON.stringify(data || {}) }}
-		/>
-	);
+	return createElement('script', {
+		id: '__ECO_PAGE_DATA__',
+		type: 'application/json',
+		dangerouslySetInnerHTML: { __html: JSON.stringify(data || {}) },
+	});
 };

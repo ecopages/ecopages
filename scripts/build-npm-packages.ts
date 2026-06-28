6b389c8645fd618f433e9b5b074e9a2ad1288b27
azanenghi <andrea.zanenghi@gmail.com>
2026-06-28 19:25:08 +0200

    refactor: remove .ts file extensions from local module imports

--- scripts/build-npm-packages.ts
+++ scripts/build-npm-packages.ts
@@ -12,13 +12,13 @@ import {
 } from 'node:fs';
 import { transform } from 'esbuild';
 import ts from 'typescript';
-import { readJsonFile, rewriteWorkspaceRanges, toPosix, type WorkspaceDependencyManifest } from './package-utils.ts';
+import { readJsonFile, rewriteWorkspaceRanges, toPosix, type WorkspaceDependencyManifest } from './package-utils';
 import {
 	assertBundledDependenciesInDist,
 	copyBundledDependenciesToDist,
 	findAllPackageDirs,
 	getBundleDependencyNames,
-} from './bundle-workspace-deps.ts';
+} from './bundle-workspace-deps';
 
 type PackageManifest = WorkspaceDependencyManifest & {
 	private?: boolean;
